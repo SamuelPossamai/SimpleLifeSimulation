11c1ac7f51c3a0f18d9a3f60a9ee43528a109552
Samuel Possamai <possamai.sam@gmail.com>
2021-02-03 18:46:52 -0300

    Created convert method for convertion

--- src/creature.py
+++ src/creature.py
@@ -200,7 +200,30 @@ class CreatureMaterialConvertionRule:
             return (f'MaterialInfo(material={repr(self.__material)}, '
                     f'quantity={self.__quantity})')
 
-    def __init__(self, name, input_list, output_list):
+    class CatalystInfo:
+
+        def __init__(self, material, effect):
+            self.__material = material
+            self.__effect = effect
+
+        @property
+        def material(self):
+            return self.__material
+
+        @property
+        def effect(self):
+            return self.__effect
+
+        def __str__(self):
+            return f'{self.__material}({self.__effect})'
+
+        def __repr__(self):
+            return (f'CatalystInfo(material={repr(self.__material)}, '
+                    f'effect={self.__effect})')
+
+    def __init__(self, name, input_list, output_list, catalysts=None,
+                 structure_multiplier=1, ingredient_multiplier=1, speed=1,
+                 join_factors_function=min):
 
         for material_info in input_list:
             material_info.material.addRule(self)
@@ -211,6 +234,50 @@ class CreatureMaterialConvertionRule:
         self.__name = name
         self.__input_list = tuple(input_list)
         self.__output_list = tuple(output_list)
+        self.__catalysts = tuple(catalysts) if catalysts is not None else None
+        self.__struct_mult = structure_multiplier
+        self.__ing_mult = ingredient_multiplier
+        self.__speed = speed
+        self.__join_func = join_factors_function
+
+    def convert(self, structure, materials, rate):
+
+        factors = [None, None, None]
+        if self.__catalysts:
+            factor = 0
+            for catalyst_info in catalyst:
+                qtd = materials.get(catalyst_info.material)
+                factor += qtd*catalyst_info.effect
+            factors[0] = factor
+
+        if self.__struct_mult:
+            factors[1] = structure*self.__struct_mult
+
+        current_qtd_for_input = tuple(materials.get(material_info.material)
+                                      for material_info in self.__input_list)
+
+        max_reactions = None
+        for qtd, material_info in zip(current_qtd_for_input, self.__input_list):
+            material_max_reactions = qtd//material_info.quantity
+            if max_reactions is None or qtd < max_reactions:
+                max_reactions = qtd
+
+        if self.__ing_mult:
+            factors[2] = max_reactions*self.__ing_mult
+
+        reactions = rate*speed*self.__join_func(
+            factor for factor in factors if factor is not None)
+
+        if reactions > max_reactions:
+            reactions = max_reactions
+
+        for qtd, material_info in zip(current_qtd_for_input, self.__input_list):
+            materials[material_info.material] = \
+                qtd - reactions*material_info.quantity
+
+        for material_info in self.__output_list:
+            materials[material_info.material] -= \
+                reactions*material_info.quantity
 
     @property
     def name(self):
